5ae1a8b8c64d72b443451c25e8a32b91c0593aee
Tej Dahal <dahaltn@Tejs-MacBook-Pro-2.local>
2020-01-27 22:21:35 +0000

    apache config and laravel folder

--- laravel/init.sh
+++ laravel/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+/bin/sed -i "s/DocumentRoot\ \/var\/www\/html/DocumentRoot\ \/var\/www\/html\/public/g" /etc/apache2/sites-available/000-default.conf
 # Start the freeradius service
 /etc/init.d/freeradius start
 
