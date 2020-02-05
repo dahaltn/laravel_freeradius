c79c81575ea0d215c44299ea69b137bda9101b6c
Tej Dahal <dahaltn@Tejs-MacBook-Pro-2.local>
2020-02-05 18:18:44 +0000

    Freeradius COA implementation

--- laravel/init.sh
+++ laravel/init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/bin/sed -i "s/DocumentRoot\ \/var\/www\/html/DocumentRoot\ \/var\/www\/html\/public/g" /etc/apache2/sites-available/000-default.conf
 # Start the freeradius service
 /etc/init.d/freeradius start
 
